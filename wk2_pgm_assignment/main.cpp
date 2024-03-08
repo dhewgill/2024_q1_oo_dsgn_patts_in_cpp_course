f391c508cd214754e3cd5b6b9eebda42d41887c5
Dale Hewgill <dale.hewgill@se.com>
2024-03-07 21:40:08 -0800

    Remove unneeded comments.

--- wk2_pgm_assignment/main.cpp
+++ wk2_pgm_assignment/main.cpp
@@ -7,10 +7,10 @@
  * @author Eric Huber - University of Illinois staff - July 2019
 **/
 
-// You need to include some header(s) here!
+
 #include <iostream>
 
-// You need to define your main() function here!
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
